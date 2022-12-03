bb1958c9cc3fb029c856f03dff72314d82fcc8fb
Mohamed Saied eltet <96622344+mohammedaltet@users.noreply.github.com>
2022-12-03 20:02:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,33 +21,29 @@ def predict(Gender, Married, Dependents, Education, Self_Employed,
     test_df.at[0,"Credit_History"] = Credit_History
     test_df.at[0,"Property_Area"] = Property_Area
     result = Model.predict(test_df)[0]
+    return result
     
-def main():
-    st.title("Loan Qualifying Procedure App")
-    st.image("""https://lines-hub.com/wp-content/uploads/2019/09/IMG_2546-1170x780.jpg""")
-    st.header('Enter the customer details:')
-    Gender = st.selectbox("Gender" , ['Male', 'Female'])
-    Married = st.selectbox("Married" , ['Yes', 'No'])
-    Dependents = st.selectbox("Dependents" , ['0', '1', '2', '3+'])
-    Education = st.selectbox("Education" , ['Graduate', 'Not Graduate'])
-    Self_Employed = st.selectbox("Self Employed or not" , ['Yes', 'No'])
-    ApplicantIncome = st.slider("Applicant Income" , min_value=150, max_value=50000, value=0, step=1)
-    CoapplicantIncome = st.slider("Coapplicant  Income" , min_value=0, max_value=20000, value=0, step=1)
-    LoanAmount = st.slider("Loan  Amount" , min_value=5000, max_value=1000000, value=0, step=1)
-    Loan_Amount_Term = st.selectbox("Loan Amount Term per day" , [360, 120, 240, 180,  60, 300, 480,  36,  84,  12])
-    Credit_History = st.selectbox("Credit History" , ['Good', 'Bad'])
-    Property_Area = st.selectbox("Property Area" , ['Urban', 'Rural', 'Semiurban'])
-    
- 
-    if st.button('applicant is eligible for a loan or not?'):
-        result = predict(Gender, Married, Dependents, Education, Self_Employed,
-        ApplicantIncome, CoapplicantIncome, LoanAmount,
-        Loan_Amount_Term, Credit_History, Property_Area)
-        pridc = predict(Gender, Married, Dependents, Education, Self_Employed,ApplicantIncome, CoapplicantIncome, LoanAmount,Loan_Amount_Term, Credit_History, Property_Area)
-        if pridc == 1:
-            pridc = 'Yes'
-        elif pridc == 0:
-            pridc = 'No'
-        st.success(pridc)
-if __name__ == '__main__':
-    main()
+
+st.title("Loan Qualifying Procedure App")
+st.image("""https://lines-hub.com/wp-content/uploads/2019/09/IMG_2546-1170x780.jpg""")
+st.header('Enter the customer details:')
+Gender = st.selectbox("Gender" , ['Male', 'Female'])
+Married = st.selectbox("Married" , ['Yes', 'No'])
+Dependents = st.selectbox("Dependents" , ['0', '1', '2', '3+'])
+Education = st.selectbox("Education" , ['Graduate', 'Not Graduate'])
+Self_Employed = st.selectbox("Self Employed or not" , ['Yes', 'No'])
+ApplicantIncome = st.slider("Applicant Income" , min_value=150, max_value=50000, value=0, step=1)
+CoapplicantIncome = st.slider("Coapplicant  Income" , min_value=0, max_value=20000, value=0, step=1)
+LoanAmount = st.slider("Loan  Amount" , min_value=5000, max_value=1000000, value=0, step=1)
+Loan_Amount_Term = st.selectbox("Loan Amount Term per day" , [360, 120, 240, 180,  60, 300, 480,  36,  84,  12])
+Credit_History = st.selectbox("Credit History" , ['Good', 'Bad'])
+Property_Area = st.selectbox("Property Area" , ['Urban', 'Rural', 'Semiurban'])
+
+
+if st.button('applicant is eligible for a loan or not?'):
+    pridc = predict(Gender, Married, Dependents, Education, Self_Employed,ApplicantIncome, CoapplicantIncome, LoanAmount,Loan_Amount_Term, Credit_History, Property_Area)
+    if pridc == 1:
+        pridc = 'Yes'
+    elif pridc == 0:
+        pridc = 'No'
+    st.success(pridc)
